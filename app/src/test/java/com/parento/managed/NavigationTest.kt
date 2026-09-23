7f07b9a302c0069c19025ff018d11cd0fe05588b
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-23 12:46:51 +0530

    test: fix and strengthen navigation coverage

--- app/src/test/java/com/parento/managed/NavigationTest.kt
+++ app/src/test/java/com/parento/managed/NavigationTest.kt
@@ -1,6 +1,7 @@
 package com.parento.managed
 
 import com.parento.managed.navigation.RootDestination
+import com.parento.managed.navigation.RootNavigator
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -8,14 +9,23 @@ class NavigationTest {
     @Test
     fun rootDestinations_areStable() {
         assertEquals(RootDestination.DEVICE_STATUS, RootDestination.valueOf("DEVICE_STATUS"))
-        assertEquals(RootDestination.ENROLLMENT_PLACEHOLDER, RootDestination.valueOf("ENROLLMENT_PLACEHOLDER"))
+        assertEquals(
+            RootDestination.ENROLLMENT_PLACEHOLDER,
+            RootDestination.valueOf("ENROLLMENT_PLACEHOLDER"),
+        )
         assertEquals(RootDestination.ERROR, RootDestination.valueOf("ERROR"))
     }
-}
 
     @Test
     fun navigator_changesDestination() {
-        val navigator = com.parento.managed.navigation.RootNavigator()
+        val navigator = RootNavigator()
         navigator.navigate(RootDestination.ERROR)
         assertEquals(RootDestination.ERROR, navigator.currentDestination)
     }
+
+    @Test
+    fun navigator_doesNotChangeDestinationWithoutNavigation() {
+        val navigator = RootNavigator()
+        assertEquals(RootDestination.DEVICE_STATUS, navigator.currentDestination)
+    }
+}
