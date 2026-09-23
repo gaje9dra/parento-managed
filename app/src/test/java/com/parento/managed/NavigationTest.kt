79c83902e5bc724f1a8bb8bdcb2bacec1a644f0f
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-23 12:11:45 +0530

    feat(phase-1.4): add managed UI shell architecture

---
+++ app/src/test/java/com/parento/managed/NavigationTest.kt
@@ -0,0 +1,14 @@
+package com.parento.managed
+
+import com.parento.managed.navigation.RootDestination
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NavigationTest {
+    @Test
+    fun rootDestinations_areStable() {
+        assertEquals(RootDestination.DEVICE_STATUS, RootDestination.valueOf("DEVICE_STATUS"))
+        assertEquals(RootDestination.ENROLLMENT_PLACEHOLDER, RootDestination.valueOf("ENROLLMENT_PLACEHOLDER"))
+        assertEquals(RootDestination.ERROR, RootDestination.valueOf("ERROR"))
+    }
+}
