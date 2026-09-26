65a4c3cf2ed91b15dfad69a6db33656fc7470e7e
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-26 09:43:15 +0530

    feat(phase-9.2): enable screen sharing feature flag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         buildConfigField("boolean", "PARENTO_FEATURE_ENROLLMENT", "true")
         buildConfigField("boolean", "PARENTO_FEATURE_REALTIME", "false")
         buildConfigField("boolean", "PARENTO_FEATURE_LOCATION", "true")
-        buildConfigField("boolean", "PARENTO_FEATURE_SCREEN_SHARING", "false")
+        buildConfigField("boolean", "PARENTO_FEATURE_SCREEN_SHARING", "true")
         buildConfigField("boolean", "PARENTO_FEATURE_AUDIO", "false")
         buildConfigField("boolean", "PARENTO_FEATURE_APPLICATION_MANAGEMENT", "false")
         buildConfigField("boolean", "PARENTO_FEATURE_WEBSITE_FILTERING", "false")
